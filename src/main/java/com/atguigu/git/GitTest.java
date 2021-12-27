1e53dd4fc4309cee24baf46b28604131a549ad27
NanJL <nanjianglei@163.com>
2021-12-27 14:27:49 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello gitnnnnnnnnnnnnnn");
+        System.out.println("hello git22222222222222222222222222222");
     }
 }
